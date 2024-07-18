5a4f1af2c725ade9e4070b115dc9f6974f66f416
avishesh karki <avishesh09401kr@gmail.com>
2024-07-18 11:41:17 +0545

    dynamic css fixation

--- src/utils/useDynamicCss.ts
+++ src/utils/useDynamicCss.ts
@@ -10,17 +10,27 @@ const useDynamicCSSImport = () => {
       // Add more URL-CSS mappings as needed
     };
 
-    // Get the corresponding CSS file based on the URL
+    // // Get the corresponding CSS file based on the URL
     const cssFile = urlToCssMap[currentURL] || "default.css"; // Default CSS if no match found
 
-    // Dynamically import the CSS file from the local folder
-    import(`../styles/${cssFile}`)
-      .then(() => {
-        console.log(`${cssFile} imported successfully`);
-      })
-      .catch((error) => {
-        console.error("Error importing CSS file:", error);
-      });
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = `/${cssFile}`;
+    document.head.appendChild(link);
+
+    // Cleanup function to remove the link element when the component unmounts
+    return () => {
+      document.head.removeChild(link);
+    };
+
+    // // Dynamically import the CSS file from the local folder
+    // import(`../styles/${cssFile}`)
+    //   .then(() => {
+    //     console.log(`${cssFile} imported successfully`);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error importing CSS file:", error);
+    //   });
   }, []); // Re-run effect if urlToCssMap changes
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Ensure the public directory is included in the build
+  publicDir: "public",
+});
